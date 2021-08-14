a55e8c57046ebb664c6d4da3dc7a3a916a669225
sebug <sebastian.gfeller@gmail.com>
2021-08-14 16:52:38 +0200

    Adding sprites

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Progremmings
 Toying around with [Kaboom.js](https://kaboomjs.com)
 
+Following the tutorial on https://www.youtube.com/watch?v=2nucjefSr6I but with my own images of course (and ideally later another game mechanic).
+
 Using GitHub pages for publishing, so the live version [can be found here](https://sebug.github.io/progremmings/).
 
--- docs/src/game.js
+++ docs/src/game.js
@@ -6,7 +6,19 @@ const k = kaboom({
     clearColor: [0,0,0,1]
 });
 
-//k.loadSprite();
+k.loadRoot('https://sebug.github.io/progremmings/img/');
+k.loadSprite('coin','coin.png');
+k.loadSprite('evil-shroom','evil-shroom.png');
+k.loadSprite('brick', 'brick.png');
+k.loadSprite('block', 'block.png');
+k.loadSprite('larry', 'larry.png');
+k.loadSprite('mushroom', 'mushroom.png');
+k.loadSprite('surprise', 'surprise.png');
+k.loadSprite('unboxed', 'unboxed.png');
+k.loadSprite('pipe-top-left', 'pipe-top-left.png');
+k.loadSprite('pipe-top-right','pipe-top-right.png');
+k.loadSprite('pipe-bottom-left', 'pipe-bottom-left.png');
+k.loadSprite('pipe-bottom-right', 'pipe-bottom-right.png');
 
 k.scene('game', () => {
     k.layers(['bg','obj', 'ui'], 'obj');
