05b311a4cd84d23b94fdffdc6dcb50847f3187e4
sebug <sebastian.gfeller@gmail.com>
2021-08-15 16:04:26 +0200

    Letting the player move

--- docs/src/game.js
+++ docs/src/game.js
@@ -6,6 +6,10 @@ const k = kaboom({
     clearColor: [0,0,0,1]
 });
 
+const JUMP_FORCE = 320;
+const MOVE_SPEED = 120;
+const FALL_DEATH = 640;
+
 k.loadRoot('https://sebug.github.io/progremmings/img/');
 k.loadSprite('coin','coin.png');
 k.loadSprite('evil-shroom','evil-shroom.png');
@@ -25,20 +29,60 @@ k.layers(['bg','obj', 'ui'], 'obj');
 k.camIgnore(['ui']);
 
 const map = [
-    '                                                 ',
-    '                                                 ',
-    '                                                 ',
-    '                                                 ',
-    '                                                 ',
-    '                                                 ',
-    '===================================  ============'
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                                                  ',
+    '                $                                 ',
+    '                                                  ',
+    '               =====                              ',
+    '                                                  ',
+    '===================================  ============='
 ];
 
 const levelCfg = {
     width: 20,
     height: 20,
-    '=': [k.sprite('block'), k.solid()]
+    '=': [k.sprite('block'), k.solid()],
+    '$': [
+	k.sprite('coin'),
+	"coin"
+	]
 };
+
+const player = k.add([
+    k.sprite('larry'),
+    k.pos(0, 0),
+    k.scale(1),
+    k.body()
+]);
+
+player.collides('coin', (c) => {
+    k.destroy(c);
+});
+
+player.action(() => {
+    k.camPos(player.pos);
+});
+
+k.keyPress('space', () => {
+    if (player.grounded()) {
+	player.jump(JUMP_FORCE);
+    }
+});
+
+k.keyDown('left', () => {
+    player.move(-MOVE_SPEED, 0);
+});
+
+k.keyDown('right', () => {
+    player.move(MOVE_SPEED, 0);
+});
 
 const gameLevel = k.addLevel(map, levelCfg);
 
