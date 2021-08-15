ce14f611ac311517cbff6f3d7a99e701e430e82c
sebug <sebastian.gfeller@gmail.com>
2021-08-15 16:27:33 +0200

    Collect coins

--- docs/src/game.js
+++ docs/src/game.js
@@ -55,6 +55,15 @@ const levelCfg = {
 	]
 };
 
+const score = k.add([
+	k.text("0"),
+	k.pos(6, 6),
+	k.layer("ui"),
+	{
+		value: 0,
+	}
+]);
+
 const player = k.add([
     k.sprite('larry'),
     k.pos(0, 0),
@@ -64,13 +73,24 @@ const player = k.add([
 
 player.collides('coin', (c) => {
     k.destroy(c);
+    score.value += 1;
+    score.text = score.value;
 });
 
 player.action(() => {
     k.camPos(player.pos);
+    if (player.pos.y >= FALL_DEATH) {
+	k.go('lose', { score: score.value });
+    }
 });
 
 k.keyPress('space', () => {
+    if (player.grounded()) {
+	player.jump(JUMP_FORCE);
+    }
+});
+
+k.keyPress('up', () => {
     if (player.grounded()) {
 	player.jump(JUMP_FORCE);
     }
@@ -82,6 +102,14 @@ k.keyDown('left', () => {
 
 k.keyDown('right', () => {
     player.move(MOVE_SPEED, 0);
+});
+
+k.scene('lose', ({ score }) => {
+    k.add([
+	k.text('Score: ' + score, 24),
+	k.origin('center'),
+	k.pos(k.width() / 2, k.height() / 2)
+    ]);
 });
 
 const gameLevel = k.addLevel(map, levelCfg);
